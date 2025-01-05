7c5f2180a535c8a592947b88c91ceaf571f8fcd2
HardeepSingh48 <137910457+HardeepSingh48@users.noreply.github.com>
2025-01-05 10:54:41 +0530

    final prod setup

--- src/main/java/com/portfolioTracker/portfolioTracker/config/WebConfig.java
+++ src/main/java/com/portfolioTracker/portfolioTracker/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.portfolioTracker.portfolioTracker.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/api/**")  // Allow CORS for /api endpoint
                 .allowedOrigins("http://localhost:5173")  // Allow only the frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed HTTP methods
